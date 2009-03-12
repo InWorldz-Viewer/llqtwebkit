36bb5ebf15909e48efd90f0b1a86a6d3ed6d6a90
unknown <Benjamin Meyer@.(none)>
2009-03-12 17:00:13 -0400

    Also install the header

--- install.bat
+++ install.bat
@@ -1,2 +1,3 @@
 cp debug\llmozlib2d.lib C:\linden\webkit\libraries\i686-win32\lib\debug
 cp release\llmozlib2.lib C:\linden\webkit\libraries\i686-win32\lib\release
+cp llmozlib2.h C:\linden\webkit\libraries\include\llmozlib2.h
