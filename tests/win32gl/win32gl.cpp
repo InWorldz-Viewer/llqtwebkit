f6802a36eb7651466ef87dccd73c4da9d6e346f5
Callum Prentice <callum@lindenlab.com>
2010-03-10 11:36:06 -0800

    Replaced the code license from Mozilla Public License with GPL v2 Most items in this commit just replace the license header block to reflect this change. Added additional license files that are explicitly called out in license header block Finally, added a simple utility batch file to build everything for Win32 users

--- tests/win32gl/win32gl.cpp
+++ tests/win32gl/win32gl.cpp
@@ -1,39 +1,27 @@
-/* ***** BEGIN LICENSE BLOCK *****
- * Version: MPL 1.1/GPL 2.0/LGPL 2.1
- *
- * The contents of this file are subject to the Mozilla Public License Version
- * 1.1 (the "License"); you may not use this file except in compliance with
- * the License. You may obtain a copy of the License at
- * http://www.mozilla.org/MPL/
- *
- * Software distributed under the License is distributed on an "AS IS" basis,
- * WITHOUT WARRANTY OF ANY KIND, either express or implied. See the License
- * for the specific language governing rights and limitations under the
- * License.
- *
- * The Original Code is Linden Lab Inc. (http://lindenlab.com) code.
- *
- * The Initial Developer of the Original Code is:
- *   Callum Prentice (callum@lindenlab.com)
- *
- * Portions created by the Initial Developer are Copyright (C) 2006
- * the Initial Developer. All Rights Reserved.
- *
- * Contributor(s):
- *
- * Alternatively, the contents of this file may be used under the terms of
- * either the GNU General Public License Version 2 or later (the "GPL"), or
- * the GNU Lesser General Public License Version 2.1 or later (the "LGPL"),
- * in which case the provisions of the GPL or the LGPL are applicable instead
- * of those above. If you wish to allow use of your version of this file only
- * under the terms of either the GPL or the LGPL, and not to allow others to
- * use your version of this file under the terms of the MPL, indicate your
- * decision by deleting the provisions above and replace them with the notice
- * and other provisions required by the GPL or the LGPL. If you do not delete
- * the provisions above, a recipient may use your version of this file under
- * the terms of any one of the MPL, the GPL or the LGPL.
- *
- * ***** END LICENSE BLOCK ***** */
+/* Copyright (c) 2006-2010, Linden Research, Inc.
+ * 
+ * LLQtWebKit Source Code
+ * The source code in this file ("Source Code") is provided by Linden Lab
+ * to you under the terms of the GNU General Public License, version 2.0
+ * ("GPL"), unless you have obtained a separate licensing agreement
+ * ("Other License"), formally executed by you and Linden Lab.  Terms of
+ * the GPL can be found in GPL-license.txt in this distribution, or online at
+ * http://secondlifegrid.net/technology-programs/license-virtual-world/viewerlicensing/gplv2
+ * 
+ * There are special exceptions to the terms and conditions of the GPL as
+ * it is applied to this Source Code. View the full text of the exception
+ * in the file FLOSS-exception.txt in this software distribution, or
+ * online at
+ * http://secondlifegrid.net/technology-programs/license-virtual-world/viewerlicensing/flossexception
+ * 
+ * By copying, modifying or distributing this software, you acknowledge
+ * that you have read and understood your obligations described above,
+ * and agree to abide by those obligations.
+ * 
+ * ALL LINDEN LAB SOURCE CODE IS PROVIDED "AS IS." LINDEN LAB MAKES NO
+ * WARRANTIES, EXPRESS, IMPLIED OR OTHERWISE, REGARDING ITS ACCURACY,
+ * COMPLETENESS OR PERFORMANCE.
+ */
 
 #include <windows.h>
 #include <gl\gl.h>
@@ -97,8 +85,7 @@ void init_qt( HWND hWnd )
     LLQtWebKit::getInstance()->flipWindow( gBrowserWindowId, false );
 
     std::string home_url;
-    home_url = "http://www.keybr.com";
-    //home_url = "file:///C:/Work/llqtwebkit-4.6/tests/testgl/testpage.html";
+    home_url = "file:///C:/Work/llqtwebkit-4.6/tests/testgl/testpage.html";
     //home_url = "http://www.keybr.com";
     //home_url = "http://www.google.com";
     //home_url = "http://www.webwasp.co.uk/tutorials/a16-input-text-frog/input-box-frog.htm";
@@ -280,10 +267,12 @@ LRESULT CALLBACK window_proc( HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam
 			// and call keyboard_event with char code.
 			gLParam = lParam;
 			gWParam = wParam;
-            if (nonprintable_key(wParam, &last_char_code)) {
+            if (nonprintable_key(wParam, &last_char_code)) 
+			{
                 // this is not a printable key
                 keyboard_event(last_char_code, LLQtWebKit::KE_KEY_DOWN);
-            } else {
+            } else 
+			{
                 // this is a printable, process in WM_CHAR
             }
             return 0;
