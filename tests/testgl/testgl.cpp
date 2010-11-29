b1b25fb09b23ebb0822bb7172368b14a51d34fb9
callum <devnull@localhost>
2010-11-29 11:42:31 -0800

    Tweaked (another) source file to remove warning messages on Windows

--- tests/testgl/testgl.cpp
+++ tests/testgl/testgl.cpp
@@ -652,7 +652,8 @@ class testGL :
 		bool onAuthRequest(const std::string &in_url, const std::string &in_realm, std::string &out_username, std::string &out_password)
 		{
 			std::cout << "Auth request, url = " << in_url << ", realm = " << in_realm << std::endl;
-			
+			out_username = "";	// replace these temporarily with site username/password as required.
+			out_password = "";
 			return false;
 		}
 
